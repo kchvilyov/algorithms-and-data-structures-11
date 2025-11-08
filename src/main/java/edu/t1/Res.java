046c038527a1186b386cb43f7de00a8ffaac2aaa
Konstantin Chvilyov <chvilyov.k@iri.center>
2025-11-08 15:17:44 +0300

    Выделил методы

--- src/main/java/edu/t1/Res.java
+++ src/main/java/edu/t1/Res.java
@@ -14,12 +14,7 @@ public class Res {
         Annotation[] annotations = clazz.getAnnotations();
         Class<?> configClass = null;
 
-        for (Annotation ann : annotations) {
-            if (ann instanceof Default) {
-                configClass = ((Default) ann).value();
-                break;
-            }
-        }
+        configClass = getConfigClass(annotations, configClass);
 
         if (configClass == null) {
             return o;
@@ -34,22 +29,36 @@ public class Res {
                         java.lang.reflect.Modifier.isFinal(modifiers)) {
                     continue;
                 }
-
-                field.setAccessible(true);
-
-                try {
-                    Field configField = configClass.getDeclaredField(field.getName());
-                    configField.setAccessible(true);
-                    Object defaultValue = configField.get(configInstance);
-                    field.set(o, defaultValue);
-                } catch (NoSuchFieldException e) {
-                    // Поле отсутствует в Config — ничего не делаем
-                }
+                setDefaultValueToField(o, field, configClass, configInstance);
             }
         } catch (Exception e) {
             throw new RuntimeException("Ошибка при установке значений по умолчания", e);
         }
 
         return o;
+    }
+
+    private static <T> void setDefaultValueToField(T o, Field field, Class<?> configClass, Object configInstance) throws NoSuchFieldException, IllegalAccessException {
+        field.setAccessible(true);
+        try {
+        // Получаем значение по умолчанию из аннотации
+            Field configField = configClass.getDeclaredField(field.getName());
+            configField.setAccessible(true);
+            // Устанавливаем значение по умолчанию в поле объекта
+            Object defaultValue = configField.get(configInstance);
+            field.set(o, defaultValue);
+        } catch (NoSuchFieldException e) {
+            // Поле отсутствует в Config — ничего не делаем
+        }
+    }
+
+    private static Class<?> getConfigClass(Annotation[] annotations, Class<?> configClass) {
+        for (Annotation ann : annotations) {
+            if (ann instanceof Default) {
+                configClass = ((Default) ann).value();
+                break;
+            }
+        }
+        return configClass;
     }
 }
